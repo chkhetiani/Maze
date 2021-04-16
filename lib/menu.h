4f5627184ea62216d97a11c1869b1272a6cc2910
sopcommand <75880117+sopcommand@users.noreply.github.com>
2021-04-16 23:40:45 +0400

    gggggggggggg

--- lib/menu.h
+++ lib/menu.h
@@ -3,22 +3,25 @@ namespace maze {
 		int level_count;
 		int max_level;
 		
-		void print_game_info() {
+		void print_game_info() {	
 		std::cout<<"The Maze"<<std::endl;
-		std::cout<<"cross the barrier and win"<<std::endl;
+		std::cout<<"Cross the barrier and won"<<std::endl;
 		}
 		
 		void print_level_list() {
 		for(int i = 1; i <= level_count; ++i){
-			std::cout<<"level: "<<max_level<<std::endl;
+			std::cout<<"level: "<<i;
+			if(max_level <= i)
+			{
+				std::cout<<"[locked]";
+			}
 		}
 		
 		}
 		
 		int select_level() {
-//			 example:
-//			 choose level to play: x
-		}
+			
+			
 		
-	};
-}
+	}
+}; // 20;30 samshabati 20 April
