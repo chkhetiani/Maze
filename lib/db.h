d2599b175c0e3d699366daef3474f75f20ad8b80
Irakli Chkhetiaini <chkhetiani.i@gmail.com>
2021-04-20 22:36:32 +0400

    add levels

--- lib/db.h
+++ lib/db.h
@@ -1,31 +1,43 @@
+#include <fstream>
 namespace maze {
 	
 	struct db {		
 		int get_max_level() {
-			return 1;
+			std::ifstream f("max_level.txt");
+			int max_level;
+			f >> max_level;
+			f.close();
+			return max_level;
 		}
 		
 		void write_max_level(int max_level) {
+			std::ofstream f("max_level.txt");
+			f << max_level;
+			f.close();
 		}
 		
 		level get_level(int number) {
-			level x;
-			x.number = 1;
-			x.height = 7;
-			x.width = 10;
-			x.start_position.x = 1;
-			x.start_position.y = 1;
-			x.goal_position.x = 4;
-			x.goal_position.y = 9;
-			x.move_count = 15;
-			x.map[0] = "##########";
-			x.map[1] = "#        #";
-			x.map[2] = "#        #";
-			x.map[3] = "#        #";
-			x.map[4] = "#         ";
-			x.map[5] = "#        #";
-			x.map[6] = "##########";
-			return x;
+			std::string file_name = "levels/" + std::to_string(number) + ".txt";
+			std::ifstream f(file_name);
+			level l;
+			f >> l.number;
+			f >> l.height;
+			f >> l.width;
+			f >> l.start_position.x;
+			f >> l.start_position.y;
+			f >> l.goal_position.x;
+			f >> l.goal_position.y;
+			f >> l.move_count;
+			
+			std::string s;
+			getline(f, s);
+			
+			for (int i = 0; i < l.height; i++) {
+				getline(f, s);
+				l.map[i] = s;
+			}
+			
+			return l;
 		}
 	};
 }
