62db31f3ac85d0fdf7074b38d6d489ebeff8800c
shakkk098 <pxaladze.2018@mail.ru>
2021-04-25 12:26:31 +0400

    Update game.h

--- lib/game.h
+++ lib/game.h
@@ -6,6 +6,25 @@ namespace maze {
 		// mizani x
 		
 		void draw_game_board() {
+			system("cls");
+			std::cout << "moves left: " << current_level.move_count << std::endl;
+			
+			for (int i = 0; i < current_level.height; i++) {
+				for (int j = 0; j < current_level.width; j++) {
+					if (current_level.goal_position.x == i 
+						&& current_level.goal_position.y == j ) {
+						std::cout << 'x';
+					}
+					else if (current_player.current_position.x == i
+						&& current_player.current_position.y == j) {
+						std::cout << current_player.icon;
+					}
+					else {
+						std::cout << current_level.map[i][j];	
+					}
+				}
+				std::cout << std::endl;
+			}
 //			example
 //          moves left: x
 //			######
@@ -16,6 +35,38 @@ namespace maze {
 		}
 		
 		char get_player_input() {
+			char move;
+			
+			while(move != 'w' && move != 'a' && move != 's' && move != 'd') {
+				move = getch();
+			}
+			
+			return move;
+		}
+		
+		void make_move(char action) {
+			
+			position pre = current_player.current_position;
+		
+			if (action == 'w') {
+				current_player.current_position.x--;
+			}
+			else if (action == 'a') {
+				current_player.current_position.y--;
+			}			
+			else if (action == 's') {
+				current_player.current_position.x++;
+			}
+			else { // d
+				current_player.current_position.y++;
+			}
+			
+			if (current_level.map[current_player.current_position.x][current_player.current_position.y] == '#') {
+				current_player.current_position = pre;
+			}
+			else {
+				current_level.move_count--;	
+			}
 //			example
 //			move with wasd
 		}
